2515a50840fba1f498d24fc9ae9cf7615e3cdc8b
Pedro Valentim <pedroavalentim@gmail.com>
2016-07-04 17:13:38 +0100

    some cleanup

--- app/client.js
+++ app/client.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom'
 import { Router, browserHistory } from 'react-router'
 import Routes from './router'
 
-console.log(Routes)
 ReactDOM.render(
   <Router history={browserHistory}>
     { Routes }
   </Router>,
   document.getElementById('app-container')
 )
-
-// ReactDOM.render(App(), document.getElementById('app-container'))
