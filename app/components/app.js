41326013ace4675a8be702d9521bdfe39695b448
Pedro Valentim <pedroavalentim@gmail.com>
2016-07-05 10:46:46 +0100

    add transitions

--- app/components/app.js
+++ app/components/app.js
@@ -1,11 +1,27 @@
 import React from 'react'
 const jade = require('react-jade')
 import { Link } from 'react-router'
-
-const template = jade.compileFile(__dirname + '/app.pug')
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 export default class AppView extends React.Component {
   render () {
-    return template.apply(this, [{ Link }])
+    return (
+      <div>
+        <ul>
+          <li><Link to={'/'}>Home</Link></li>
+          <li><Link to={'/about'}>About</Link></li>
+        </ul>
+        <div id='app-view'>
+          <ReactCSSTransitionGroup
+            transitionName='example'
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}>
+              { React.cloneElement(this.props.children, { key: this.props.location.pathname }) }
+          </ReactCSSTransitionGroup>
+        </div>
+
+      </div>
+    )
+    // return template.apply(this, [{ Link }])
   }
 }
